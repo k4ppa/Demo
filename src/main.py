aae69c5fc431543615131dd2748f182b8498b371
k4ppa <cappa.mattia@gmail.com>
2015-04-17 15:15:24 +0200

    Refactoring

--- src/main.py
+++ src/main.py
@@ -3,76 +3,7 @@ from mobile_framework.android_device import AndroidDevice
 
 import stormtest.ClientAPI as StormTest
 from assistance_menu import AssistanceMenu
-from stormtest.ClientAPI import PressButton
-
-   
-
-def connectToTheNetwork(galaxyTab3, assistanceMenu):
-    goToSettings(assistanceMenu)
-    goToWiFi(galaxyTab3)
-    galaxyTab3.tap(text='Vodafone-WIFI')
-    enterPassword()
-    galaxyTab3.tap(text='Connect')
-    StormTest.WaitSec(5)
-    closeSettings(assistanceMenu)
-    pass
-
-
-def goToSettings(assistanceMenu):
-    assistanceMenu.open()
-    assistanceMenu.up()
-    assistanceMenu.settings()
-    pass
-
-
-def goToWiFi(galaxyTab3):
-    PressButton("SWIPE:220:110:220:760:0")
-    PressButton("SWIPE:220:110:220:760:0")
-    PressButton("SWIPE:220:110:220:760:0")
-    StormTest.WaitSec(1)
-    galaxyTab3.tap(text='Wi-Fi')
-    StormTest.WaitSec(1)
-    pass
-
-
-def enterPassword():
-    StormTest.WaitSec(2)
-    PressButton("TAP:55:680")
-    StormTest.WaitSec(1)
-    PressButton("TAP:55:680")
-    StormTest.WaitSec(1)
-    
-    for i in range(3):
-        PressButton("TAP:570:600:0")
-        StormTest.WaitSec(1)
-        PressButton("TAP:790:600:0")
-        StormTest.WaitSec(1)
-        PressButton("TAP:860:680:0")
-        StormTest.WaitSec(1)
-    pass
-
-
-
-def disconnectToTheNetwork(galaxyTab3, assistanceMenu):
-    goToSettings(assistanceMenu)
-    goToWiFi(galaxyTab3)
-    galaxyTab3.tap(text='Vodafone-WIFI')
-    StormTest.WaitSec(2)
-    galaxyTab3.tap(text='Forget')
-    StormTest.WaitSec(5)
-    closeSettings(assistanceMenu)
-    pass
-
-
-def closeSettings(assistanceMenu):
-    assistanceMenu.open()
-    StormTest.WaitSec(1)
-    assistanceMenu.down()
-    StormTest.WaitSec(1)
-    assistanceMenu.back()
-    StormTest.WaitSec(1)
-    assistanceMenu.close()
-    pass
+from network_functions import connectToTheNetwork, disconnectToTheNetwork
 
 
 if __name__ == '__main__':
@@ -83,8 +14,12 @@ if __name__ == '__main__':
     StormTest.WaitSec(3)
     
     connectToTheNetwork(galaxyTab3, assistanceMenu)
-    disconnectToTheNetwork(galaxyTab3, assistanceMenu)
     
+    
+    
+    
+    
+    disconnectToTheNetwork(galaxyTab3, assistanceMenu)
     StormTest.WaitSec(1)
     galaxyTab3.stop()
     pass
