7462c14fced4ae283f322dc07cb032701499338e
k4ppa <cappa.mattia@gmail.com>
2015-04-17 15:09:38 +0200

    Disconnection with wi-fi network

--- src/main.py
+++ src/main.py
@@ -5,29 +5,23 @@ import stormtest.ClientAPI as StormTest
 from assistance_menu import AssistanceMenu
 from stormtest.ClientAPI import PressButton
 
+   
 
-
-
-
-
-
-def restoreDefaultState(assistanceMenu):
-    assistanceMenu.open()
-    StormTest.WaitSec(1)
-    assistanceMenu.down()
-    StormTest.WaitSec(1)
-    assistanceMenu.close()
+def connectToTheNetwork(galaxyTab3, assistanceMenu):
+    goToSettings(assistanceMenu)
+    goToWiFi(galaxyTab3)
+    galaxyTab3.tap(text='Vodafone-WIFI')
+    enterPassword()
+    galaxyTab3.tap(text='Connect')
+    StormTest.WaitSec(5)
+    closeSettings(assistanceMenu)
     pass
 
 
-def closeSettings(assistanceMenu):
+def goToSettings(assistanceMenu):
     assistanceMenu.open()
-    StormTest.WaitSec(1)
-    assistanceMenu.down()
-    StormTest.WaitSec(1)
-    assistanceMenu.back()
-    StormTest.WaitSec(1)
-    assistanceMenu.close()
+    assistanceMenu.up()
+    assistanceMenu.settings()
     pass
 
 
@@ -37,13 +31,7 @@ def goToWiFi(galaxyTab3):
     PressButton("SWIPE:220:110:220:760:0")
     StormTest.WaitSec(1)
     galaxyTab3.tap(text='Wi-Fi')
-    pass
-
-
-def goToSettings(assistanceMenu):
-    assistanceMenu.open()
-    assistanceMenu.up()
-    assistanceMenu.settings()
+    StormTest.WaitSec(1)
     pass
 
 
@@ -62,7 +50,29 @@ def enterPassword():
         PressButton("TAP:860:680:0")
         StormTest.WaitSec(1)
     pass
-    
+
+
+
+def disconnectToTheNetwork(galaxyTab3, assistanceMenu):
+    goToSettings(assistanceMenu)
+    goToWiFi(galaxyTab3)
+    galaxyTab3.tap(text='Vodafone-WIFI')
+    StormTest.WaitSec(2)
+    galaxyTab3.tap(text='Forget')
+    StormTest.WaitSec(5)
+    closeSettings(assistanceMenu)
+    pass
+
+
+def closeSettings(assistanceMenu):
+    assistanceMenu.open()
+    StormTest.WaitSec(1)
+    assistanceMenu.down()
+    StormTest.WaitSec(1)
+    assistanceMenu.back()
+    StormTest.WaitSec(1)
+    assistanceMenu.close()
+    pass
 
 
 if __name__ == '__main__':
@@ -72,27 +82,8 @@ if __name__ == '__main__':
     assistanceMenu = AssistanceMenu()
     StormTest.WaitSec(3)
     
-    
-    goToSettings(assistanceMenu)
-    goToWiFi(galaxyTab3)
-    
-    galaxyTab3.tap(text='Vodafone-WIFI')
-    
-    '''
-    if galaxyTab3.tap(text='Forget'):
-        StormTest.WaitSec(2)
-        galaxyTab3.tap(text='Vodafone-WIFI')
-    '''
-    
-    enterPassword()
-    galaxyTab3.tap(text='Connect')
-    
-    StormTest.WaitSec(5)
-    closeSettings(assistanceMenu)
-    
-    #StormTest.WaitSec(5)
-    #restoreDefaultState(assistanceMenu)
-    
+    connectToTheNetwork(galaxyTab3, assistanceMenu)
+    disconnectToTheNetwork(galaxyTab3, assistanceMenu)
     
     StormTest.WaitSec(1)
     galaxyTab3.stop()
